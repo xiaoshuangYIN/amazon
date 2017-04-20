e90661b69a488ebee870986c3899b35af17a516e
Bitnami <bitnami@ubuntu16-generic-template-01.oit.duke.edu>
2017-04-20 17:32:31 -0400

    write Apack

--- GPB.h
+++ GPB.h
@@ -1,9 +1,11 @@
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/text_format.h>
-#include "protocal/amazon_orig_2.pb.h"
+
+//#include "protocal/amazon_orig_2.pb.h"
+
 #include <iostream>
 
 template<typename T> bool sendMesgTo(const T & , google::protobuf::io::FileOutputStream *);
-template<typename T> bool recvMesgFrom(T & message, google::protobuf::io::FileInputStream * in );
+template<typename T> bool recvMesgFrom(T & , google::protobuf::io::FileInputStream * );
 //std::ostream & operator<<(std::ostream & s, const google::protobuf::Message & m);
--- amazon_server.cpp
+++ amazon_server.cpp
@@ -1,5 +1,5 @@
 #include "server_base.h"
-#include "GPB.h"
+//#include "GPB.h"
 #include "protocal/amazon_orig_2.pb.h"
 
 #include <google/protobuf/io/coded_stream.h>
@@ -8,48 +8,60 @@
 
 #include <string>
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 const char* sim_IP = "10.236.48.17";
 const char* PORT = "23456";
-
 template<typename T> bool sendMesgTo(const T & message, google::protobuf::io::FileOutputStream *out) {
   {
     //extra scope: make output go away before out->Flush()
+    // We create a new coded stream for each message.  Don't worry, this is fast.
     google::protobuf::io::CodedOutputStream output(out);
+    // Write the size.
     const int size = message.ByteSize();
     output.WriteVarint32(size);
     uint8_t* buffer = output.GetDirectBufferForNBytesAndAdvance(size);
     if (buffer != NULL) {
+      // Optimization:  The message fits in one buffer, so use the faster
+      // direct-to-array serialization path.
       message.SerializeWithCachedSizesToArray(buffer);
     } else {
+      // Slightly-slower path when the message is multiple buffers.
       message.SerializeWithCachedSizes(&output);
       if (output.HadError()) {
 	return false;
       }
     }
   }
+
+  //std::cout << "flushing...\n";
   out->Flush();
   return true;
 }
 
 template<typename T> bool recvMesgFrom(T & message,
 				       google::protobuf::io::FileInputStream * in ){
-
+  // We create a new coded stream for each message.  Don't worry, this is fast,
+  // and it makes sure the 64MB total size limit is imposed per-message rather
+  // than on the whole stream.  (See the CodedInputStream interface for more
+  // info on this limit.)
   google::protobuf::io::CodedInputStream input(in);
-
+  // Read the size.
   uint32_t size;
   if (!input.ReadVarint32(&size)) {
-    std::cout<<"read size false, exit......\n";
     return false;
   }
-  printf("receive size : %d\n", size);
+  // Tell the stream not to read beyond that size.
   google::protobuf::io::CodedInputStream::Limit limit = input.PushLimit(size);
+  // Parse the message.
   if (!message.MergeFromCodedStream(&input)) {
     return false;
   }
   if (!input.ConsumedEntireMessage()) {
     return false;
   }
+  // Release the limit.
   input.PopLimit(limit);
   return true;
 }
@@ -70,32 +82,41 @@ int main(int argc, char* argv[]){
   connect_sock(&servinfo, sockfd,  s);
 
   /* google protoco buffer */
-  /* write connect message */
+  /* AConnect */
   AConnect conn;
-  conn.set_worldid(1043);
-  //AInitWarehouse* initWh = conn.add_initwh();
-  //initWh->set_x(-664);
-  //initWh->set_y(-1081);
- 
+  conn.set_worldid(1043);// worldid = the value after init world
 
   google::protobuf::io::FileOutputStream * outfile = new google::protobuf::io::FileOutputStream(sockfd);
-  bool suc = sendMesgTo(conn, outfile);
-  if (suc == false){
+  if ( sendMesgTo(conn, outfile) == false){
     std::cout<<"amazon server: Aconnect fail to send\n";
   }
 
   /* receive */
   AConnected connRes;
   google::protobuf::io::FileInputStream * infile = new google::protobuf::io::FileInputStream(sockfd);
-  suc = recvMesgFrom(connRes, infile);
-  if (suc == false){
+  if (recvMesgFrom(connRes, infile)== false){
     std::cout<<"amazon server: Aconnected fail to recv\n";
   }
 
+
+  /* APack */
+  /* fetch from db */
+  int whNum = 1;
+  int shipId = 1;
+  std::vector<std::unordered_map<std::string, std::string> > products;
+  /* ****** */
+  APack pack;
+  pack.set_whnum(whNum);
+  pack.set_shipid(shipId);
+  AProduct* prod;
+  for(int i = 0; i < products.size(); i++){
+    prod = pack.add_things();
+    prod->set_id((std::stoi((products[i])["id"]), NULL));
+    prod->set_description((products[i])["description"]);
+    prod->set_count((std::stoi((products[i])["count"]), NULL));
+  }
   
   /* test */
-  //AConnect connTest;
-  //connTest.ParseFromString(*output);
   if (connRes.has_error() ){ 
     printf("response: %s\n",connRes.error().c_str());
   }
